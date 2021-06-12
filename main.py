2ab25fae19763eae8e14edcac46787eadaddfc58
nikifox <63959903+nikifox@users.noreply.github.com>
2021-06-12 13:12:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,29 @@ import discord
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix=".")
+bot = commands.Bot(command_prefix=".")
 token = os.getenv("DISCORD_BOT_TOKEN")
 
-@client.event
+@bot.event
 async def on_ready() :
-    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+    await bot.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("I am online")
 
-@client.command()
+@bot.command()
 async def ping(ctx) :
     await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
 
-@client.command(name="whoami")
+@bot.command(name="whoami")
 async def whoami(ctx) :
     await ctx.send(f"You are {ctx.message.author.name}")
 
-@client.command()
+@bot.command()
 async def clear(ctx, amount=3) :
     await ctx.channel.purge(limit=amount)
 
     
-with open('FoxBot/Core_Code/RUN.py')as SF:source=SF.read()
+with open('m2.py')as SF:source=SF.read()
 exec(source)
 
 
-client.run(token)
+bot.run(token)
